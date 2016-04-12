5f0fdddcffda5bdf5a56f6b4cf55b860dab72137
Fengtianhe <545704061@qq.com>
2016-04-13 00:09:03 +0800

    创建用户表

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -1,8 +1,10 @@
 <?php
 namespace Admin\Controller;
 use Think\Controller;
-class IndexController extends Controller {
+class IndexController extends CommonController {
     public function index(){
+    	//$this->display('login');
+
     	$url['xml_url'] = __ROOT__."/Public/admin/dwz.frag.xml";
     	$url['login'] = U('Admin/admin/login');
     	$this->assign('url',$url);
