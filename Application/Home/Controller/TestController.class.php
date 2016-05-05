fbc1bd396c676bd8787ddc9ec9bb99e952929d34
my1977 <1977905246@qq.com>
2016-05-05 19:37:24 +0800

    模拟考试 控制器

---
+++ Application/Home/Controller/TestController.class.php
@@ -0,0 +1,29 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class TestController extends CommonController {
+    public function _initialize (){
+        parent::_initialize();
+    }
+    public function index(){
+        $this->display();
+    }
+    public function startTest(){
+        $category = I('get.category');
+        $question = D('Question')->getSimpleQuestionByWhere(array('category'=>$category));
+        $this->assign('question', $question);
+        $this->display();
+    }
+    public function ajaxGetQuestion(){
+        $id = I('post.id');
+        $question_info = D('Question')->getInfoById($id);
+    	$result['question_info'] = $question_info;
+    	$result['status'] = $status;
+        $this->ajaxReturn($result);
+    }
+    public function handlePractice(){
+    	//$user_id = I('session.me.id');
+    	$user_id 		= $_SESSION['me']['id'];
+    	$question_id 	= I('post.id');
+    }
+}
