b68dea53d1058e3992c95797d95f418b1b5c8213
my1977 <1977905246@qq.com>
2016-05-12 16:54:25 +0800

    手机端模板

--- Application/Home/Controller/CommonController.class.php
+++ Application/Home/Controller/CommonController.class.php
@@ -3,6 +3,9 @@ namespace Home\Controller;
 use Think\Controller;
 class CommonController extends Controller {
     public function _initialize(){
+    	if (is_mobile_request()) {
+    		C('DEFAULT_THEME')  = 'mobile';
+    	}
     	if (check_replay_login()) {
     		unset($_SESSION['me']);
     		$this->error("您的账号在其他设备登录，若不是本人操作请修改密码或联系管理员",'/');
