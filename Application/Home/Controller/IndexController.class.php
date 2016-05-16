803642cf8ee52db46ca407dbec80d80cfc9d71d7
Fengtianhe <545704061@qq.com>
2016-05-16 11:47:09 +0800

    手机端首页，pc用户中心大框

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -6,6 +6,14 @@ class IndexController extends CommonController {
 		parent::_initialize();
 	}
     public function index(){
+    	$program=M();
+		$strSql="select max(create_time) as max from ks_question ";
+		$res=$program->query($strSql);
+		$maxtime= $res[0]['max'];
+
+		$question = M('question');
+		// $dan_list = $question->where(array(''))->find();
+		$this->assign("maxtime",$maxtime);
      	$this->display('main');
     }
 }
