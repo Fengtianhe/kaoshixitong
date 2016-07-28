04e11bb6f2bc928d7464e3c1683cc146e805a428
my1977 <1977905246@qq.com>
2016-07-28 14:31:05 +0800

    用户充值日志

---
+++ Application/Admin/Model/UserLogModel.class.php
@@ -0,0 +1,11 @@
+<?php
+namespace Admin\Model;
+use Think\Model;
+class UserLogModel extends Model {
+	public function addLog($data){
+		$this->addLog($data);
+	}
+	public function transformData($data){
+		return $data;
+	}
+}
