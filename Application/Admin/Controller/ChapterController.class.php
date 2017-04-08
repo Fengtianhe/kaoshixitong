518f35db4c6561edc384975764374ee769c02a0f
shuaizongjie <1042602614@qq.com>
2017-04-08 14:30:05 +0800

    增加章节模块

---
+++ Application/Admin/Controller/ChapterController.class.php
@@ -0,0 +1,17 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+class ChapterController extends CommonController {
+    public function _initialize(){
+        $this->checkLogin();
+    }
+    public function lists(){
+        
+        $this->display();
+    }
+    
+
+    
+
+    
+}
